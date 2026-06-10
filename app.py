450e009110692a8b389e4484855eed7c88904bd5
LUSKAS <lukasbiasotto710@Gmail.com>
2026-06-10 09:06:19 -0300

    satanel o segundo

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Olá, mundo! Este é o documento padrão do Flask."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

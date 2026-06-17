da36f37f242246c16fd432f3315305fbf6a6cab5
EuliteralmenteEu <gustavocordeiroovni@gmail.com>
2026-06-17 11:19:34 -0300

    lilililililillillillillillii

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def get_db():
     """Abre uma conexão com o banco de dados se já não houver uma ativa na requisição."""
     if 'db' not in g:
         g.db = mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_DATABASE
+            host=localhost,
+            user=root,
+            password=,
+            database=almoxarifado
         )
     return g.db
 

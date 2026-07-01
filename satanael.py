d4573a50a512fede10733862652d86e7c6e3b31c
demarchiandre2020-del <demarchiandre2020@gmail.com>
2026-07-01 11:10:19 -0300

    Update satanael.py

--- satanael.py
+++ satanael.py
@@ -34,8 +34,8 @@ def close_db(e):
 
 #--------------ROTAS BACANAS---------------
 @app.route("/")
-def Ganesha():
-    return render_template("Ganesha.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/ABRAXAS")
 def ABRAXAS():
